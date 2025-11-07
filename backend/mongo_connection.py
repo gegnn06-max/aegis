ae8c4df5a6af53ef464f7af8a67c344c06be9d85
gegnn06-max <gegnn06@gmail.com>
2025-11-07 13:25:20 +0530

    Update mongo_connection.py

--- backend/mongo_connection.py
+++ backend/mongo_connection.py
@@ -12,13 +12,8 @@ from datetime import datetime
 load_dotenv()
 
 app = Flask(__name__)
-CORS(
-    app,
-    resources={r"/": {"origins": ""}},
-    supports_credentials=True,
-    allow_headers=["Content-Type", "Authorization"],
-    methods=["GET", "POST", "OPTIONS", "PUT", "DELETE"]
-)
+CORS(app)
+
 app.config['JSON_SORT_KEYS'] = False
 
 def get_db():
